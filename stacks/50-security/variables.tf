0760614c93375015a714015189fd2d2993c41355
yun1270 <modo000127@gmail.com>
2025-09-08 17:18:17 +0900

    chore(terraform): iam 수정

--- stacks/50-security/variables.tf
+++ stacks/50-security/variables.tf
@@ -6,27 +6,9 @@ variable "tags" {
   default     = {}
 }
 
-variable "account_id"              { type = string }
-
-variable "service_definitions" {
-  description = "Per-service SG definition (ingress from ALB, and specific egress to DB/Cache)"
-  type = map(object({
-    port         = number
-    egress = list(object({
-      to    = string
-      port  = number
-      proto = optional(string, "tcp")
-    }))
-  }))
-}
 
 variable "secret_names" { type = map(string) }
 variable "kms_key_arn"  { type = string }
-
-variable "vpc_cidr" {
-  description = "VPC CIDR 블록"
-  type        = string
-}
 
 variable "eks_cluster_name"   { type = string }
 variable "namespace"          { type = string }
