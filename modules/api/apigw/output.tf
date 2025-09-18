c2c0a9973b5ae845b0953434d3c41474a5343a7b
yun1270 <modo000127@gmail.com>
2025-09-18 10:54:59 +0900

    chore(terraform): 파일 구조 수정

--- modules/api/apigw/output.tf
+++
@@ -1,7 +0,0 @@
-output "api_id"        { value = aws_api_gateway_rest_api.this.id }
-# output "stage_name"    { value = aws_api_gateway_stage.this.stage_name }
-# output "stage_arn"     { value = aws_api_gateway_stage.this.arn }
-# output "invoke_url"    {
-#   value = "https://${aws_api_gateway_rest_api.this.id}.execute-api.${data.aws_region.current.name}.amazonaws.com/${aws_api_gateway_stage.this.stage_name}"
-# }
-# output "vpc_link_id"   { value = aws_api_gateway_vpc_link.this.id }
--- modules/api/waf/output.tf
+++
@@ -1,1 +0,0 @@
-# output "web_acl_arn" { value = aws_wafv2_web_acl.this.arn }
